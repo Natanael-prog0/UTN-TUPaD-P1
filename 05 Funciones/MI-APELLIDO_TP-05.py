43ea0bb1819a7b4e05b408b960ca1673299bf9b7
Natanael_Caliva <calivanatanaelmdz@gmail.com>
2025-06-13 22:16:13 -0300

    Update MI-APELLIDO_TP-05.py

---
+++ 05 Funciones/MI-APELLIDO_TP-05.py
@@ -0,0 +1,6 @@
+Trabajo Práctico: Funciones
+Alumno: Natanael Caliva
+Comisión: 1
+
+Descripción
+Este trabajo práctico tiene como objetivo la implementación y práctica de funciones en Python. Se desarrollaron distintos ejercicios para consolidar los conceptos fundamentales sobre la definición, invocación, parámetros y retorno de funciones.
